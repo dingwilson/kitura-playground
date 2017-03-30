92814d0e2571c71c8a04c91a2d8d18247c986cfb
Wilson Ding <shodai100@gmail.com>
2017-03-30 00:07:15 -0500

    Added HeliumLogger

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "kitura-playground",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6)
     ]
 )
